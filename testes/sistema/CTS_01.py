5c4849b1f8184936e3298a52ebc5524422c841a4
armandossrecife <armando.sousa@gmail.com>
2024-11-02 02:02:41 -0300

    Ajustes na documentação mostrando como executar os testes

--- testes/sistema/CTS_01.py
+++ testes/sistema/CTS_01.py
@@ -39,7 +39,6 @@ class TestLoginFeature(unittest.TestCase):
             #assert 'Credenciais inválidas' in self.driver.page_source
 
         def tearDown(self):
-            self.driver.close()
             self.driver.quit()
 
 if __name__ == "__main__":
